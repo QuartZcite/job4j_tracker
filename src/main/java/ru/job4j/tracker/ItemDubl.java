60d8cfec43e4a0bbb60f5ab35fda4792329a72d5
QuartZcite <wulfdogster@gmail.com>
2020-06-30 00:42:07 +0300

    2. Перегрузка конструктора. [#301000]

---
+++ src/main/java/ru/job4j/tracker/ItemDubl.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+public class ItemDubl {
+
+    private String  id;
+    private String name;
+
+    public ItemDubl() {
+    }
+
+    public ItemDubl(String id) {
+    }
+
+    public ItemDubl(String id, String name) {
+    }
+
+
+
+    public static void main(String[] args) {
+        Item item = new Item();
+    }
+}
